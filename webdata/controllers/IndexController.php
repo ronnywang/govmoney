de3df8634c3d8ac928ce1539d91c385f60de886c
Ronny Wang <ronnywang@gmail.com>
2021-06-17 20:56:05 +0800

    增加首頁依照組織顯示

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -5,4 +5,14 @@ class IndexController extends Pix_Controller
     public function indexAction()
     {
     }
+
+    public function unitAction()
+    {
+        list(, /*index*/, /*unit*/, $unit_name) = explode('/', $this->getURI());
+        $unit_name = urldecode($unit_name);
+        if (!$unit = Unit::find_by_name($unit_name)) {
+            return $this->redirect('/');
+        }
+        $this->view->unit = $unit;
+    }
 }

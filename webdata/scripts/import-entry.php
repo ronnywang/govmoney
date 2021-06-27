a786db78fad2aaf18422de4d9b953b23a013faf3
Ronny Wang <ronnywang@gmail.com>
2021-06-27 17:25:32 +0800

    改用 Entry::getEntryIdOrCreateIt

--- webdata/scripts/import-entry.php
+++ webdata/scripts/import-entry.php
@@ -4,22 +4,13 @@ include(__DIR__ . '/../init.inc.php');
 
 Pix_Setting::set('Table:DropTableEnable', true);
 Entry::dropTable();
-Entry::createTable();
 EntryValue::dropTable();
+Entry::createTable();
 EntryValue::createTable();
-
-$unit_map = new StdClass;
-foreach (Unit::search(1) as $unit) {
-    $data = json_decode($unit->data);
-    foreach ($data->year_id as $year_id2 => $id2) {
-        $unit_map->{$year_id2} = $unit->id;
-    }
-}
 
 $fp = gzopen(__DIR__ . '/預算案-歲出機關別預算表.csv.gz', 'r');
 $cols = fgetcsv($fp);
 
-$unit_item = new StdClass;
 $names = array();
 while ($rows = fgetcsv($fp)) {
     $values = array_combine($cols, $rows);
@@ -39,60 +30,29 @@ while ($rows = fgetcsv($fp)) {
     $year = $values['年'];
     $id = $values['科目編號'];
     $id1 = substr($id, 0, 2);
-    $id2 = intval(substr($id, 2, 4));
     $id3 = substr($id, 6, 2);
     $id4 = substr($id, 8, 2);
-
-    if (!$unit_id = $unit_map->{$year  . '-' . $id2}) {
-        print_r($values);
-        throw new Exception("找不到 {$year}-{$id2} 的單位");
-    }
-    error_log("{$year}-{$id2} => {$unit_id}");
 
     if ($id1 == '00' and $id3 == '00' and $id4 == '00') {
         continue;
     } else {
-        if ($id3 == '00' and $id4 == '00') {
-            $names = array(0, $values['科目名稱']);
-            $index = 0;
-        } else if ($id4 == '00') {
-            $names = array(0, $names[1], $values['科目名稱']);
-            $index = 1;
-        } else {
-            $names[3] = $values['科目名稱'];
-            $index = 2;
-        }
-
-
-        $key = "{$unit_id}-{$names[$index]}-{$names[$index + 1]}";
-        error_log($key);
-
-        if (property_exists($unit_item, $key)) {
-            $entry = $unit_item->{$key};
-            $names[$index + 1] = $entry->entry_id;
-            $data = json_decode($entry->data);
-            $data->year_id->{"{$year}-{$id}"} = $id;
-            $entry->update(array(
-                'data' => json_encode($data),
+        $entry_id = Entry::getEntryIdOrCreateIt($year, $id, $values['科目名稱']);
+        error_log("{$year}-{$id} {$values['科目名稱']} => $entry_id");
+        try {
+            EntryValue::insert(array(
+                'entry_id' => $entry_id,
+                'category' => '預算案',
+                'time' => $year,
+                'value' => intval($values['本年預算數']),
+                'data' => json_encode(array(
+                    '說明' => $values['說明'],
+                )),
             ));
-        } else {
-            $entry = Entry::insert(array(
-                'unit_id' => $unit_id,
-                'parent_id' => $names[$index],
-                'name' => $names[$index + 1],
-                'data' => json_encode(array('year_id' => array("{$year}-{$id}" => $id))),
-            ));
-            $names[$index + 1] = $entry->entry_id;
-            $unit_item->{$key} = $entry;
+        } catch (Pix_Table_DuplicateException $e) {
+            print_r($values);
+            print_r(EntryValue::search(array('entry_id' => $entry_id, 'category' => '預算案', 'time' => $year))->toArray());
+            print_r(Entry::find($entry_id)->toArray());
+            exit;
         }
-        EntryValue::insert(array(
-            'entry_id' => $entry->entry_id,
-            'category' => '預算案',
-            'time' => $year,
-            'value' => intval($values['本年預算數']),
-            'data' => json_encode(array(
-                '說明' => $values['說明'],
-            )),
-        ));
     }
 }
